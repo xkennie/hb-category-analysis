0737d9accbcd2cbf7cf24e45801a0d33411f0e68
xkennie <120515141+xkennie@users.noreply.github.com>
2024-08-14 12:30:40 +0300

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -265,8 +265,7 @@ if uploaded_file is not None:
     '#5f9ea0',
     '#6495ed',
     '#4169e1',
-    '#b0e0e6',
-    '#add8e6'
+    '#b0e0e6'
 ]) 
     st.write(csv_file1)  
     st.write("Список топовых товаров в лучшем ценовом сегменте:")
