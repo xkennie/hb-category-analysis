d3cf2b1b02b4cb164fda03ae4de7e018988921a7
xkennie <120515141+xkennie@users.noreply.github.com>
2024-08-14 12:55:46 +0300

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -244,7 +244,8 @@ if uploaded_file is not None:
     st.subheader("Выбор ценового сегмента")
     
     t = price_segmentation(data_category_preprocess(df_from_file, category_filter))
-    st.bar_chart(t, x = "Диапазон", y = "Коэффициент", color = "Ценовой сегмент") 
+    d = t.sort_values(by = "Средняя цена")
+    st.bar_chart(d, x = "Диапазон", y = "Коэффициент", color = "Ценовой сегмент") 
     #Niche Analysis
     Range_name = "Эконом"
     Range_name = st.selectbox(
